319cebe0cba39d30eae5d5b170fcc9809f7c4787
nguyenthiensinh <upworksinh@enjoyworks.co.kr>
2021-07-18 19:48:21 +0700

    end project

--- index.php
+++ index.php
@@ -1,39 +1,10 @@
-
-<!-- include head.html -->
-<title>Meta title for web</title>
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta http-equiv="Content-type" content="text/html;charset=UTF-8">
 <!-- include head.html -->
 <title>Meta title for web</title>
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-<link rel="stylesheet" type="text/css" media="screen" href="assets/styles/style.css" />
-<h5>KetCauSoftTheme_SiteMap</h5>
-<ol>
-  <li><a href="index.html">Main_ketcausoft</a></li>
-  <li>
-    <a href="index_authentication.html">Main_authentication_ketcausoft</a>
-  </li>
-  <li><a href="calculation_software.html">Calculation_software</a></li>
-  <li><a href="drawing_software.html">Drawing_software</a></li>
-  <li><a href="examination_final.html">Examination_final</a></li>
-  <li><a href="examination_question.html">Examination_question</a></li>
-  <li><a href="examination.html">Examination</a></li>
-  <li><a href="free_softeware.html">Free_softeware</a></li>
-  <li><a href="pfd_software.html">Pfd_software</a></li>
-  <li><a href="post_library_ketcausoft.html">Post_library_ketcausoft</a></li>
-  <li>
-    <a href="document_library_ketcausoft.html">Document_library_ketcausoft</a>
-  </li>
-  <li><a href="video_library_ketcausoft.html">Video_library_ketcausoft</a></li>
-  <li><a href="forum_library_ketcausoft.html">Forum_library_ketcausoft</a></li>
-  <li><a href="search_standard.html">search_standard</a></li>
-  <li><a href="search.html">search</a></li>
-  <li><a href="search_reuse.html">search_reuse</a></li>
-</ol>
-
-<link rel="stylesheet" type="text/css" media="screen" href="assets/styles/style.css" />
+<link rel="stylesheet" href="https://danaweb.vn/css/mmenu.css">
+<link rel="stylesheet" type="text/css" media="screen" href="assets/styles/style.css?v=203154721357003" />
 <h5>KetCauSoftTheme_SiteMap </h5>
 <strong>SITE</strong>
 <ol>
@@ -55,11 +26,18 @@
   <li><a href="education_02_ketcausoft.html">Education_02_ketcausoft</a></li>
   <li><a href="education_03_ketcausoft.html">Education_03_ketcausoft</a></li>
   <li><a href="education_04_ketcausoft.html">Education_04_ketcausoft</a></li>
+  <li><a href="education_05_ketcausoft.html">Education_05_ketcausoft</a></li>
+  <li><a href="education_06_ketcausoft.html">Education_06_ketcausoft</a></li>
+  <li><a href="download_ketcausoft.html">Download_ketcausoft</a></li>
   <li><a href="search_standard.html">Search_standard</a></li>
   <li><a href="search.html">Search</a></li>
   <li><a href="search_reuse.html">Search_reuse</a></li>
-  <li><a href="book_constructor.html">Book_constructor</a></li>
-  <li><a href="book_detail.html">Book_detail</a></li>
+  <li><a href="book_constructor.html">book_constructor</a></li>
+  <li><a href="book_detail.html">book_detail</a></li>
+  <li><a href="buy_soft.html">buy_soft</a></li>
+  <li><a href="user_ketcausoft_01.html">ketcausoft-User-CP 01</a></li>
+  <li><a href="user_ketcausoft_02.html">ketcausoft-User-CP 02</a></li>
+  <li><a href="user_ketcausoft_03.html">ketcausoft-User-CP 03</a></li>
 </ol>
 
 <strong>ADMIN</strong>
